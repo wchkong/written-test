0ee733c372c2f6891cbba1fe0db504cf1a73f647
Kongweichang <1017074319@qq.com>
2019-09-17 09:34:20 +0800

    fix

--- src/main/java/t10guazi/Main.java
+++ src/main/java/t10guazi/Main.java
@@ -4,46 +4,69 @@ import java.util.Scanner;
 
 /**
  * @Author: kongweichang
- * @Date: 2019/9/16 14:43
+ * @Date: 2019/9/16 15:56
  */
 public class Main {
-
-    static Integer count = 0;
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        String string = in.nextLine();
-        char[] chars = string.toCharArray();
-        getCount(chars, 0);
-        System.out.println(count);
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        int K = sc.nextInt();
+
+        String min = S;
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            String newStr = move(S, getMaxCharAt(S, K));
+            int compare = compare(newStr, min);
+            if (compare == 1) {
+                min = newStr;
+            }
+            S = newStr;
+        }
+        System.out.println(min);
+
+    }
+
+    public static int getMaxCharAt(String str, int K) {
+
+        if (K > str.length()) {
+            K = str.length();
+        }
+        int maxIndex = 0;
+        char charMax = 'a' - 1;
+        for (int i = 0; i < K; i++) {
+            if (str.charAt(i) > charMax) {
+                charMax = str.charAt(i);
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 
 
-    public static void getCount(char[] chars, int i) {
+    public static String move(String str, int charAt) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        char c = stringBuilder.charAt(charAt);
+        stringBuilder.deleteCharAt(charAt);
+        stringBuilder.append(c);
+        return stringBuilder.toString();
+    }
 
-        if (i == chars.length) {
-            count ++;
-            return;
+    public static int compare(String s1, String s2) {
+
+        char[] chars = s1.toCharArray();
+        char[] chars1 = s2.toCharArray();
+        if (chars.length != chars1.length) {
+            throw new RuntimeException("长度不同");
         }
 
-        if (chars[i] == '0') {
-            getCount(chars, i + 1);
-        }
-        if (chars[i] == '1') {
-            getCount(chars, i + 1);
-            if (i + 1 < chars.length) {
-                getCount(chars, i + 2);
+        for (int i = 0; i < chars.length; i++) {
+            if (chars1[i] > chars[i]) {
+                return 1;
+            } else if (chars1[i] < chars[i]) {
+                return -1;
             }
         }
-        if (chars[i] == '2') {
-            getCount(chars, i + 1);
-            if (i + 1 < chars.length && (chars[i + 1] >= '0' && chars[i + 1] <= '6')) {
-                getCount(chars, i + 2);
-            }
-        }
-        if (chars[i] >= '3') {
-            getCount(chars, i + 1);
-        }
+        return 0;
     }
 }
