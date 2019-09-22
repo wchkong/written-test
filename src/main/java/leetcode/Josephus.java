01949a359a87e49a0f5eb93a9f9db4958a6c8229
kongweichang <kongweichang@qq.com>
2019-09-22 22:23:22 +0800

    Josephus

---
+++ src/main/java/leetcode/Josephus.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * Josephus problem
+ * 约瑟夫问题
+ * 丢手绢问题
+ *
+ * 在一间房间总共有n个人（下标0～n-1），只能有最后一个人活命。
+ *
+ * 按照如下规则去杀人：
+ * 所有人围成一圈
+ * 顺时针报数，每次报到q的人将被杀掉
+ * 被杀掉的人将从房间内被移走
+ * 然后从被杀掉的下一个人重新报数，继续报q，再清除，直到剩余一人
+ *
+ *
+ * 输入
+ * 人的个数 : n
+ * 每次报到q 就会被杀死 的 q
+ *
+ * 输出
+ * 最终能够活下来的人的下标
+ */
+public class Josephus {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int q = scanner.nextInt();
+
+        // f(n) = (f(n - 1) + q) % n
+        // 最後存活index為0的人 即 f(n) = 0; 倒推
+        if (n == 0) {
+            return;
+        }
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = (result + q) % i;
+        }
+        // 0 - n-1
+        System.out.println(result);
+    }
+
+}
