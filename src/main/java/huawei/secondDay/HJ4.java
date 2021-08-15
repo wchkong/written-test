5ffbd0b9c30e6c7de9e0f34711bf8f33f10d42cc
kongweichang <kongweichang@qq.com>
2021-08-15 19:02:11 +0800

    add score2

--- src/main/java/huawei/secondDay/HJ4.java
+++ src/main/java/huawei/secondDay/HJ4.java
@@ -1,5 +1,7 @@
 package huawei.secondDay;
 
+import java.util.Scanner;
+
 /**
  * @author kongweichang
  * @date 2021/8/15 6:41 下午
@@ -8,6 +10,21 @@ package huawei.secondDay;
  **/
 public class HJ4 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            char[] chars = s.toCharArray();
+            int i = chars.length / 8;
+            char[][] chars1 = new char[chars.length % 8 > 0 ? i + 1 : i][8];
+            for (int j = 0; j < chars.length; j++) {
+                chars1[j/8][j%8] = chars[j];
+            }
+            for (int j = 0; j < chars1.length; j++) {
+                for (int k = 0; k < 8; k++) {
+                    System.out.print(chars1[j][k]);
+                }
+                System.out.println();
+            }
+        }
     }
 }
